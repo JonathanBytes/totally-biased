c0727226a2605a05dfeb9d8b6343c7924ccdad5f
Jonathan ArchDell <jonathancardonaramirez@gmail.com>
2025-09-07 18:26:45 -0500

    Enhance styles

--- components/SetupPanel.tsx
+++ components/SetupPanel.tsx
@@ -6,6 +6,7 @@ import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import { Button } from "./ui/button";
 import { toast } from "sonner";
+import { CornerDownLeft } from "lucide-react";
 
 interface SetupPanelProps {
   className?: string;
@@ -60,7 +61,9 @@ const SetupPanel: React.FC<SetupPanelProps> = (
   }
 
   return (
-    <div className={`${className} flex items-center flex-col max-w-2xl`}>
+    <div
+      className={`${className} flex items-center flex-col max-w-2xl h-screen`}
+    >
       <p className="text-left w-full">
         Totally biased is a{" "}
         <span className="font-mono text-sm dark:bg-neutral-700 dark:text-neutral-300 bg-cyan-400 text-neutral-700 rounded px-1 size-0.5">
@@ -82,9 +85,16 @@ const SetupPanel: React.FC<SetupPanelProps> = (
           onKeyDown={handleKeyDown}
           autoFocus
         />
-        <div className="flex justify-end">
-          <Button className="w-fit" type="submit" onClick={handleStartClick}>
+        <div className="flex justify-end items-center gap-2">
+          <p className="text-sm text-neutral-500">Ctrl + Enter</p>
+          <Button
+            variant="default"
+            className="h-fit py-0.5 shadow-md shadow-cyan-500/50 dark:shadow-cyan-500/30 border-2 border-neutral-900 hover:border-neutral-700 dark:border-neutral-400 cursor-pointer flex items-center justify-center gap-2"
+            type="submit"
+            onClick={handleStartClick}
+          >
             Start
+            <CornerDownLeft />
           </Button>
         </div>
       </div>
